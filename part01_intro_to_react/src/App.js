d36df09b98d5426c169d0612df74139e1a64ee0c
Jonathan Mani <jonathanmani@protonmail.com>
2022-09-18 16:45:38 -0400

    created a component

--- part01_intro_to_react/src/App.js
+++ part01_intro_to_react/src/App.js
@@ -1,14 +1,27 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
+const Hello = (props) => {
+  const bornYear = () => {    
+    const yearNow = new Date().getFullYear()    
+    return yearNow - props.age 
+  }
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
       <p>
-        {a} plus {b} is {a + b}
+        Hello {props.name}, you are {props.age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 26
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} /> 
+      <Hello name={name} age={age} />   
     </div>
   )
 }
