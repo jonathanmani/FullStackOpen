770218a807ffada0e6c9e285cc885b6a09a679bc
Jonathan Mani <jonathanmani@protonmail.com>
2022-09-19 20:15:07 -0400

    implement button component and states for good,bad and neutral -- unicafe part 01

--- part01_intro_to_react/src/App.js
+++ part01_intro_to_react/src/App.js
@@ -1,49 +1,82 @@
 import { useState } from "react";
 
-const History = (props) => {  
-  if (props.allClicks.length === 0) {
-    return (      
-      <div>        
-        the app is used by pressing the buttons      
-      </div>    
-    )  
-  }  
-  
-  return (    
-    <div>      
-      button press history: {props.allClicks.join(' ')}    
-    </div>  
-  )
-}
-
-const Button = ({ handleClick, text }) => (  
+const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
-    {text}  
+    {text}
   </button>
 )
 
-
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const handleLeftClick = () => { setAll(allClicks.concat('L'))    
-    setLeft(left + 1)  
-  }
-  const handleRightClick = () => { setAll(allClicks.concat('R'))    
-    setRight(right + 1)  
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const goodClicks = () => setGood(good + 1)
+  const neutralClicks = () => setNeutral(neutral + 1)
+  const badClicks = () => setBad(bad + 1)
 
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks ={allClicks} />    
+      <h1>give feedback</h1>
+      <Button handleClick={goodClicks} text='good' />
+      <Button handleClick={neutralClicks} text='neutral' />
+      <Button handleClick={badClicks} text='bad' />
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
+
+// const History = (props) => {  
+//   if (props.allClicks.length === 0) {
+//     return (      
+//       <div>        
+//         the app is used by pressing the buttons      
+//       </div>    
+//     )  
+//   }  
+//   return (    
+//     <div>      
+//       button press history: {props.allClicks.join(' ')}    
+//     </div>  
+//   )
+// }
+
+// const Button = ({ handleClick, text }) => (  
+//   <button onClick={handleClick}>
+//     {text}  
+//   </button>
+// )
+
+
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+
+//   const handleLeftClick = () => { 
+//     setAll(allClicks.concat('L'))    
+//     setLeft(left + 1)  
+//   }
+
+//   const handleRightClick = () => { 
+//     setAll(allClicks.concat('R'))    
+//     setRight(right + 1)  
+//   }
+
+//   return (
+//     <div>
+//       {left}
+//       <Button handleClick={handleLeftClick} text='left' />
+//       <Button handleClick={handleRightClick} text='right' />
+//       {right}
+//       <History allClicks ={allClicks} />    
+//     </div>
+//   )
+// }
   
 
 export default App;
