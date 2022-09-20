d7eb466a452a9d8508427c69dae34c2d90510d7f
Jonathan Mani <jonathanmani@protonmail.com>
2022-09-19 20:31:22 -0400

    added additional state and modified that state to display average and percentage of positive reviews -- unicafe part 02

--- part01_intro_to_react/src/App.js
+++ part01_intro_to_react/src/App.js
@@ -11,9 +11,19 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const goodClicks = () => setGood(good + 1)
-  const neutralClicks = () => setNeutral(neutral + 1)
-  const badClicks = () => setBad(bad + 1)
+  const [all, setAll] = useState(0)
+  const goodClicks = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }  
+  const neutralClicks = () => { 
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+  const badClicks = () => { 
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
 
   return (
     <div>
@@ -26,6 +36,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {all}</p>
+      <p>average {all / 3}</p>
+      <p>positive {good / all} % </p>
     </div>
   )
 }
